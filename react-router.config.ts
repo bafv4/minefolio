88ebe233f6020c146e8baf3ce3a7f23ac0a5be6e
baf <yuanqiwei71@gmail.com>
2026-01-19 15:12:12 +0900

    (3)

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,11 +3,4 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: true,
   serverModuleFormat: "esm",
-  presets: [
-    // Vercel deployment preset
-    async () => {
-      const { vercelPreset } = await import("@vercel/remix");
-      return vercelPreset();
-    },
-  ],
 } satisfies Config;
